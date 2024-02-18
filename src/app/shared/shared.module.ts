9d0eeb34cf15f343a414d6271c3af3aa0ea1af92
chernyshv <vchernysh@mobilunity.com>
2024-02-18 17:46:39 +0200

    remove unused import

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,7 +5,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSliderModule } from '@angular/material/slider';
-import { MatInputModule } from '@angular/material/input';
 
 import { FilterArrayComponent } from './components/filters/filter-array/filter-array.component';
 import { FilterTextComponent } from './components/filters/filter-text/filter-text.component';
@@ -24,13 +23,11 @@ import { FilterNumberComponent } from './components/filters/filter-number/filter
     MatTableModule,
     MatPaginatorModule,
     MatSliderModule,
-    MatInputModule,
   ],
   exports: [
     MatTableModule,
     MatPaginatorModule,
     MatSliderModule,
-    MatInputModule,
 
     FilterArrayComponent,
     FilterTextComponent,
